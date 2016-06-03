aa06cf0aa6ffcca3d710c12c3c019d556a5bc95c
Dawid Nowak <nowakd@gmail.com>
2016-06-03 23:32:57 +0100

    Spark+Cassandra Docker combo

---
+++ docker-entrypoint.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -e
+
+
+echo "Docker Spark Bootstrap $SPARK_RUNNING"
+
+if [ "$SPARK_RUNNING" = "true" ]
+   then
+       echo "Spark already started $SPARK_RUNNING"
+else
+    ip addr
+    export SPARK_RUNNING="true"
+    if [ "$SPARK_MODE" = "master" ]
+    then
+	echo "Running as Master"
+	/opt/spark-latest/sbin/start-master.sh
+    else
+	echo "Running as Worker $SPARK_MASTER_IP"
+	/opt/spark-latest/sbin/start-slave.sh $SPARK_MASTER_IP:7077
+    fi
+fi
+
+
+
+
+# first arg is `-f` or `--some-option`
+if [ "${1:0:1}" = '-' ]; then
+	set -- cassandra -f "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
+	chown -R cassandra /var/lib/cassandra /var/log/cassandra "$CASSANDRA_CONFIG"
+	exec gosu cassandra "$BASH_SOURCE" "$@"
+fi
+
+if [ "$1" = 'cassandra' ]; then
+	: ${CASSANDRA_RPC_ADDRESS='0.0.0.0'}
+
+	: ${CASSANDRA_LISTEN_ADDRESS='auto'}
+	if [ "$CASSANDRA_LISTEN_ADDRESS" = 'auto' ]; then
+		CASSANDRA_LISTEN_ADDRESS="$(hostname --ip-address)"
+	fi
+
+	: ${CASSANDRA_BROADCAST_ADDRESS="$CASSANDRA_LISTEN_ADDRESS"}
+
+	if [ "$CASSANDRA_BROADCAST_ADDRESS" = 'auto' ]; then
+		CASSANDRA_BROADCAST_ADDRESS="$(hostname --ip-address)"
+	fi
+	: ${CASSANDRA_BROADCAST_RPC_ADDRESS:=$CASSANDRA_BROADCAST_ADDRESS}
+
+	if [ -n "${CASSANDRA_NAME:+1}" ]; then
+		: ${CASSANDRA_SEEDS:="cassandra"}
+	fi
+	: ${CASSANDRA_SEEDS:="$CASSANDRA_BROADCAST_ADDRESS"}
+	
+	sed -ri 's/(- seeds:) "127.0.0.1"/\1 "'"$CASSANDRA_SEEDS"'"/' "$CASSANDRA_CONFIG/cassandra.yaml"
+
+	for yaml in \
+		broadcast_address \
+		broadcast_rpc_address \
+		cluster_name \
+		endpoint_snitch \
+		listen_address \
+		num_tokens \
+		rpc_address \
+		start_rpc \
+	; do
+		var="CASSANDRA_${yaml^^}"
+		val="${!var}"
+		if [ "$val" ]; then
+			sed -ri 's/^(# )?('"$yaml"':).*/\2 '"$val"'/' "$CASSANDRA_CONFIG/cassandra.yaml"
+		fi
+	done
+
+	for rackdc in dc rack; do
+		var="CASSANDRA_${rackdc^^}"
+		val="${!var}"
+		if [ "$val" ]; then
+			sed -ri 's/^('"$rackdc"'=).*/\1 '"$val"'/' "$CASSANDRA_CONFIG/cassandra-rackdc.properties"
+		fi
+	done
+fi
+
+exec "$@"
